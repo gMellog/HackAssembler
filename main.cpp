f0513539e2ff6b966a46a2817194defe5f2cb349
Sviatoslav <80694229+gMellog@users.noreply.github.com>
2022-05-23 11:53:31 +0300

    Todos were deleted

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
-// HackAssembler.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-
-//First thing is to create file + just pass for in and out files when try run it on bash
-//next make simple assembly, when there is just instructions 
-//then make it with variables
-//then make it with symbols
-//done
 
 #include "HackAssembler.h"
 
